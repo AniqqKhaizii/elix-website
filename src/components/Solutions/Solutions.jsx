366e606783681de4798d374ba0213ad4d8ef87ae
Aniqq Khaizii <aniqqkhaizii@gmail.com>
2024-07-01 14:54:09 +0800

    Second Commit 1/7/2024

--- src/components/Solutions/Solutions.jsx
+++ src/components/Solutions/Solutions.jsx
@@ -5,24 +5,24 @@ import solution2 from '../../assets/images/data-integration.jpg'
 import solution3 from '../../assets/images/cloud.jpg'
 
 const Solutions = () => {
-  return (
-    <div className="container">
-        <div className='solutions'>
-            <div className="solution">
-                <img src={solution1} alt='' />
-                <p>Sistem Penguatkuasaan Bersepadu (SPKB)</p>
-            </div>
-            <div className="solution">
-                <img src={solution2} alt='' />
-                <p>Data Integration</p>
-            </div>
-            <div className="solution">
-                <img src={solution3} alt='' />
-                <p>Cloud Services</p>
+    return (
+        <div className="container">
+            <div className='solutions'>
+                <div className="solution">
+                    <img src={solution1} alt='' />
+                    <p>Sistem Penguatkuasaan Bersepadu (SPKB)</p>
+                </div>
+                <div className="solution">
+                    <img src={solution2} alt='' />
+                    <p>Data Integration</p>
+                </div>
+                <div className="solution">
+                    <img src={solution3} alt='' />
+                    <p>Cloud Services</p>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Solutions
