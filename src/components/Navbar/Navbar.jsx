0844f6bc8f0f94d43cfe443536d366a420eeb72c
Aniqq Khaizii <aniqqkhaizii@gmail.com>
2024-07-02 17:55:21 +0800

    Fifth  Commit 2/7/2024

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,22 +5,22 @@ const Navbar = () => {
 
   const [sticky, setSticky] = useState(false);
 
-  useEffect(()=>{
-    window.addEventListener('scroll', ()=>{
-      window.scrollY > 50 ? setSticky(true) : setSticky (false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
     })
-  },[])
+  }, [])
 
   return (
-      <nav className={`container ${sticky? 'dark-nav' : ''}`}>  
-        <img className="nav-logo" src={logo} alt='/' />
-        <ul className="nav-menu">
-              <li>Home</li>
-              <li>Solution</li>
-              <li>Products</li>
-              <li className='nav-contact'>Contact</li>
-        </ul>
-      </nav>
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+      <img className="nav-logo" src={logo} alt='/' />
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About</li>
+        <li>Solutions</li>
+        <li className='nav-contact'>Contact</li>
+      </ul>
+    </nav>
   )
 }
 
